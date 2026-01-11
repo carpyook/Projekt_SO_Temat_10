58bbe838206fe76363b8e6252a17ddb91508c6aa
Mateusz <carpyook002@gmail.com>
2026-01-12 00:17:11 +0100

    Dodanie obsługi semaforów: inicjalizacja, funkcje P V i usuwanie

--- common.h
+++ common.h
@@ -18,7 +18,7 @@
 
 typedef struct {
     char type;
-    char weight;
+    int weight;
     int volume;
     pid_t worker_id;
 } Package;
@@ -34,6 +34,44 @@ typedef struct {
 static void check_error(int ret, const char *msg) {
     if (ret == -1) {
         perror(msg);
+        exit(EXIT_FAILURE);
+    }
+}
+
+// union wymagany przez funkcje semctl
+union semun {
+    int val;
+    struct semid_ds *buf;
+    unsigned short *array;
+};
+
+//nazwy semaforow
+enum SemType {
+    SEM_MUTEX = 0, // Dostep do pamieci (binarny 0/1)
+    SEM_EMPTY = 1, // Liczba wolnych miejsc
+    SEM_FULL  = 2  // Liczba zajetych miejsc (paczek)
+};
+
+//funkcja P
+static void sem_wait(int sem_id, int sem_num) {
+    struct sembuf op;
+    op.sem_num = sem_num;
+    op.sem_op = -1;
+    op.sem_flg = 0;
+    if (semop(sem_id, &op, 1) == -1) {
+        perror("Blad sem_wait");
+        exit(EXIT_FAILURE);
+    }
+}
+
+//funkcja V
+static void sem_signal(int sem_id, int sem_num) {
+    struct sembuf op;
+    op.sem_num = sem_num;
+    op.sem_op = 1; // Dodaj 1
+    op.sem_flg = 0;
+    if (semop(sem_id, &op, 1) == -1) {
+        perror("Blad sem_signal");
         exit(EXIT_FAILURE);
     }
 }
--- main.c
+++ main.c
@@ -19,8 +19,34 @@ belt->tail = 0;
 belt->current_count = 0;
 belt->current_weight = 0;
 
+//zestaw 3 semaforow
+int sem_id = semget(SEM_KEY, 3, IPC_CREAT | 0666);
+check_error(sem_id, "error semget");
+printf("[info] Semafory utworzone ID: %d\n", sem_id);
+
+union semun arg;
+
+// 1. MUTEX = 1 (Dostep do tasmy)
+arg.val = 1;
+check_error(semctl(sem_id, SEM_MUTEX, SETVAL, arg), "blad init MUTEX");
+
+// 2. EMPTY = MAX_BUFFER_SIZE (Na starcie cala tasma pusta)
+arg.val = MAX_BUFFER_SIZE;
+check_error(semctl(sem_id, SEM_EMPTY, SETVAL, arg), "blad init EMPTY");
+
+// 3. FULL = 0 (Na starcie zero paczek)
+arg.val = 0;
+check_error(semctl(sem_id, SEM_FULL, SETVAL, arg), "blad init FULL");
+
+printf("[info] Semafory ustawione: MUTEX=1, EMPTY=%d, FULL=0\n", MAX_BUFFER_SIZE);
+
+//SYMULACJA
 printf("[info] tasma jest pusta\n");
 sleep (5);
+
+//usuwamy semafory
+check_error(semctl(sem_id, 0, IPC_RMID), "blad usuwania semaforow");
+printf("[info] Semafory usuniete\n");
 
 check_error(shmdt(belt), "blad shmdt"); // odlaczenie wskaznika
 
