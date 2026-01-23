033322149e6a30bcc72e280e655fc79f5ea550ab
Mateusz <carpyook002@gmail.com>
2026-01-23 14:09:28 +0100

    Dodanie obslugi SIGTERM i flagi shutdown w worker

--- worker.c
+++ worker.c
@@ -1,14 +1,35 @@
+#define _POSIX_C_SOURCE 200809L // sigaction
 #include "common.h"
 #include <time.h>
+#include <signal.h>
+
+volatile sig_atomic_t should_exit = 0;
+
+void handle_sigterm(int sig) {
+    (void)sig;
+    should_exit = 1;
+}
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        printf("uzyj ./worker [TYP A/B/C]\n");
+        fprintf(stderr, "Uzycie: %s [TYP A/B/C]\n", argv[0]);
         return 1;
 
     }
     char type = argv[1][0]; // pobranie pierwszej litery
+    if (type != 'A' && type != 'B' && type != 'C') {
+        fprintf(stderr, "[BLAD] Nieprawidlowy typ paczki: %c\n", type);
+        return 1;
+    }
+
     srand(time(NULL) ^ getpid());
+
+    struct sigaction sa;
+    sa.sa_handler = handle_sigterm;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGTERM, &sa, NULL);
+
 
     printf("[P%c] pracownik uruchomiony, PID: %d\n", type, getpid());
 
@@ -16,17 +37,27 @@ int main(int argc, char *argv[]) {
 
     // pamiec
     int shm_id = shmget(SHM_KEY, sizeof(SharedBelt), 0);
-    check_error(shm_id, "Worker: blad shmget");
+    if (shm_id == -1) {
+        perror("Worker: shmget");
+        return 1;
+    }   
 
     SharedBelt *belt = (SharedBelt *)shmat(shm_id, NULL, 0);
-    if (belt == (void *)-1) check_error(-1, "Worker: blad shmat");
+    if (belt == (void *)-1) {
+        perror("Worker: shmat");
+        return 1;
+    }
 
     // semafory
     int sem_id = semget(SEM_KEY, 4, 0);
-    check_error(sem_id, "Worker: blad semget");
+    if (sem_id == -1) {
+        perror("Worker: semget");
+        shmdt(belt);  // clean up
+    return 1;
+    }   
 
     // petla pracy
-    while (1){
+    while (!belt->shutdown && !should_exit) {
         // genorowanie paczki
         Package pkg;
         pkg.type = type;
@@ -51,16 +82,27 @@ int main(int argc, char *argv[]) {
         
 
         // czekaj na wolne miejsce(tasma pelna = stop)
-        sem_wait(sem_id, SEM_EMPTY);
+        if (sem_wait_wrapper(sem_id, SEM_EMPTY) == -1) {
+            if (should_exit || belt->shutdown) break;
+            continue;
+        }
+
+        //sprawdzenie flagi
+        if (belt->shutdown) {
+            sem_signal(sem_id, SEM_EMPTY);
+            break;
+        }
 
         //czekaj na dostep do pamieci(tylko jeden proces na raz moze pisac)
-        sem_wait(sem_id, SEM_MUTEX);
+        if (sem_wait_wrapper(sem_id, SEM_MUTEX) == -1) {
+            sem_signal(sem_id, SEM_EMPTY);
+            if (should_exit || belt->shutdown) break;
+            continue;
+        }
     
         // sprawdzenie udzwigu tasmy (limit M)
         if (belt->current_weight + pkg.weight > MAX_WEIGHT_BELT) {
-            printf("[P%c] BLAD: Tasma przeciazona! (%.1ff + %.1f >  %.1f). Czekam...\n", 
-                 type, belt->current_weight, pkg.weight, MAX_WEIGHT_BELT);
-                
+            printf("[P%c] Tasma przeciazona! Czekam...\n", type);          
             
             // oddanie semaforow, zeby ciezarowka mogla cos zdjac
             sem_signal(sem_id, SEM_MUTEX);
@@ -89,6 +131,9 @@ int main(int argc, char *argv[]) {
         
        sleep(rand() % 3 + 1); // symulacja pracy (1-3 sekundy przerwy)
     }
+    
+    printf("[P%c] Koniec pracy. \n", type);
+    shmdt(belt);
 
     return 0;
 
